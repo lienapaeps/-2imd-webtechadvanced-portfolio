f100c2a0887117654809f0fb1b006b173a87accf
lienapaeps <paepsliena@gmail.com>
2022-03-15 20:28:00 +0100

    feat: add working connection to second API

--- lab4/classes/App.js
+++ lab4/classes/App.js
@@ -4,6 +4,7 @@ export default class App {
         this.lng = 0;
         this.API_KEY = API_KEY;
         this.getLocation();
+        this.getHeroes();
     }
 
     // geolocatie opvragen -> mdn geolocation API
@@ -29,7 +30,7 @@ export default class App {
         console.log("Getting weather");
         // API call
         let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${this.lat}&lon=${this.lng}&appid=${this.API_KEY}`;
-        // console.log(url);
+        console.log(url);
 
         // MDN fetch API
         fetch(url).then( response => {
@@ -52,13 +53,79 @@ export default class App {
         let maxTemp = Math.round(json.main.temp_max);
         let minTemp = Math.round(json.main.temp_min);
 
-        // console.log(summary);
-        // console.log(temp);
 
         document.querySelector(".summary").innerHTML = summary;
         document.querySelector(".temp").innerHTML = temp + "°";
         document.querySelector(".place").innerHTML = place;
         document.querySelector(".max").innerHTML = "max: " + maxTemp + "°";
         document.querySelector(".min").innerHTML = "min: " + minTemp + "°";
+
+        // data teruggeven
+        localStorage.getItem(place, summary, temp);
+
+        let description = [];
+        description.push(place)
+        description.push(summary)
+        description.push(temp)
+        localStorage.setItem("weather", JSON.stringify(description));
+
+        description = localStorage.getItem("weather");
+        json = JSON.parse(weather);
+
+        // tweede API
+
+    }
+
+    // Marvel API
+    getHeroes() {
+        // API key = 6045a091342bd776dd0213c064c8c027
+        console.log("Getting heroes");
+        // API call
+        // Spider-Man (Ultimate)
+        let url = 'https://gateway.marvel.com:443/v1/public/characters?name=Spider-Man%20(Ultimate)&apikey=f84f7431e98ce48579540c00d38f21f5';
+        // console.log(url);
+
+        // MDN fetch API
+        fetch(url).then( response => {
+            return response.json(); // antwoord omzetten naar json
+        } ).then( json => {
+            // hier iets met json gaan doen
+            this.printHeroes(json);
+            console.log(json);
+        } ).catch( error => {
+            console.log(error);
+        }).finally( () => {
+            console.log("finally done");
+        } );
+    }
+
+    printHeroes(json) {
+        // let summary = json.superhero[0].description;
+        let copyright = json.copyright;
+        let name = json.data.results[0].name;
+        let path = json.data.results[0].thumbnail.path;
+        let extension = json.data.results[0].thumbnail.extension;
+
+        let thumbnail = path + "." + extension;
+
+        document.querySelector(".name").innerHTML = name;
+
+        document.querySelector("#app").style.backgroundImage = `url('${thumbnail}')`;
+        document.querySelector("#app").style.backgroundSize = "cover";
+        document.querySelector("#app").style.backgroundRepeat = "no-repeat";
+
+        document.querySelector(".copyright").innerHTML = copyright;
+
+        // data teruggeven
+        // localStorage.getItem(name);
+
+        // let description = [];
+        // description.push(name)
+        // localStorage.setItem("superhero", JSON.stringify(description));
+
+        // description = localStorage.getItem("superhero");
+        // json = JSON.parse(superhero);
     }
 }
+
+// 6e1cac7a40779b5ce2a25191e7eaf33d94677c26
